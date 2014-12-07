7faeada151048ada33dceec9843a4207f7d07571
root <root@ip-172-31-39-244.ec2.internal>
2014-12-07 20:16:23 +0000

    Skeleton of time range API call

--- app.py
+++ app.py
@@ -31,6 +31,36 @@ def test():
 
     return output
 
+# Current time ranges
+# 2014-02-19 18:13:00 to 2014-02-25 23:36:59
+# 2014-02-26 00:17:00 to 2014-03-04 23:59:59
+# 2014-03-05 00:00:00 to 2014-03-11 23:59:59
+# 2014-03-12 00:00:00 to 2014-03-18 23:59:59
+# 2014-03-19 00:00:00 to 2014-03-25 23:59:59
+# 2014-03-26 00:00:00 to 2014-04-01 23:59:59
+# 2014-04-02 00:00:00 to 2014-04-08 23:59:59
+# 2014-04-09 00:00:00 to 2014-04-09 03:31:59
+@app.route("/time_range/<int:time_range_index>")
+def getDataInTimeRange(timeRangeIndex):
+    if timeRange == 1:
+        pass
+    elif timeRange == 2:
+        pass
+    elif timeRange == 3:
+        pass
+    elif timeRange == 4:
+        pass
+    elif timeRange == 5:
+        pass
+    elif timeRange == 6:
+        pass
+    elif timeRange == 7:
+        pass
+    else:
+        # Return some kind of error message
+        pass
+	
+
 if __name__ == '__main__':
     # Set to false before deployment
     # Allows server to update on code changes without having to restart it
