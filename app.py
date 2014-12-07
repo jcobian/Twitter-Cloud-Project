6fd2786ce212b6e4cb40c347959e22d0229d18e1
root <root@ip-172-31-39-244.ec2.internal>
2014-12-07 19:45:51 +0000

    Comments added

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 # -*- mode: python -*-
+#
+# Core app code that launches the actual application
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+#------------------------------------------------------------------------
+# Database connection and setup
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://masteruser:_cloud2014@cloudprojectdb.cqwak4ataijt.us-east-1.rds.amazonaws.com:3306/cloudproject'
 db = SQLAlchemy(app)
 
@@ -12,14 +18,21 @@ class Tweet(db.Model):
     __tablename__ = 'Tweet'
     __table_args__ = {'autoload':True, 'autoload_with':db.engine}
 
+#TODO: Add other tables here
+
+#------------------------------------------------------------------------
+# URL Routing
+
 @app.route("/")
-def hello():
+def test():
     output = ''; 
     for item in db.session.query(Tweet).filter(Tweet.Favorites > 20):
-        output += str(item.TweetID) + '\n'
+        output += str(item.TweetID) + '<br>'
 
     return output
 
 if __name__ == '__main__':
+    # Set to false before deployment
+    # Allows server to update on code changes without having to restart it
     app.debug = True
     app.run(host='0.0.0.0')
