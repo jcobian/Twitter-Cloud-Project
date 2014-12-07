c4ba2ffc670f8d6a4fe1d2a20221296605f1ee16
jcobian <jcobian@nd.edu>
2014-12-07 02:33:43 -0500

    check if multiple keys being mapped more than once
    added code to combiner to make sure a key isn’t mapped more than once

--- combiner.py
+++ combiner.py
@@ -6,11 +6,17 @@ def main():
 	end_day = sys.argv[2]
 	limit = int(sys.argv[3])
 	results = []
+	usedKeys = set()
 	for line in sys.stdin:
 		tokens = line.split("\t")
 		key = tokens[0]
-		value = int(tokens[1])
-		results.append((key,value))
+		if key not in usedKeys:
+			value = int(tokens[1])
+			results.append((key,value))
+			usedKeys.add(key)
+		else:
+			print "Error: %s in file more than once" % key
+			sys.exit()
 
 	results = sorted(results,key=lambda x:x[1],reverse=True)
 	results = results[:limit]
