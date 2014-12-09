fdf2172e799d926f149cb48e687f33e4fb011ee1
Maribeth Rauh <maribeth.rauh@gmail.com>
2014-12-09 07:30:56 +0000

    Data being sent to client

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+import json
 
 app = Flask(__name__)
 
@@ -40,11 +41,24 @@ db.session.commit()
 #-----------------------------------------------------------------------
 # Queries to database
 
+tweetLocations = list()
+
 def getInfoForDateRange(startTime, endTime):
-	keywords = db.session.query(TopKeyword).filter(TopKeyword.StartRange == startTime, TopKeyword.EndRange == endTime)
-	for keyword in keywords:
-		print keyword.Keyword
-	return keywords
+	keywords = list()
+	for keyword in db.session.query(TopKeyword).filter(TopKeyword.StartRange == startTime, TopKeyword.EndRange == endTime):
+		keywords.append(keyword.Keyword)
+	hashtags = list()
+	for hashtag in db.session.query(TopHashtag).filter(TopHashtag.StartRange == startTime, TopHashtag.EndRange == endTime):
+		hashtags.append(hashtag.Hashtag)
+	images = list()
+	for image in db.session.query(TopImage).filter(TopImage.StartRange == startTime, TopImage.EndRange == endTime):
+		images.append(image.Image)
+	links = list()
+	for link in db.session.query(TopLink).filter(TopLink.StartRange == startTime, TopLink.EndRange == endTime):
+		links.append(link.Link)
+	for location in db.session.query(Tweet).filter(Tweet.Lat != None, Tweet.Lng != None, Tweet.Time > startTime, Tweet.Time < endTime):
+		tweetLocations.append({'lat': str(location.Lat), 'lng': str(location.Lng)})
+	return keywords, hashtags, images, links
 
 #------------------------------------------------------------------------
 # URL Routing
@@ -62,25 +76,29 @@ def getInfoForDateRange(startTime, endTime):
 def getDataInTimeRange(timeRangeIndex=1):
     timeRangeIndex = int(timeRangeIndex)
     if timeRangeIndex == 1:
-	topKeywords = getInfoForDateRange('2014-02-19 00:00:00', '2014-02-25 23:59:59')
-
-    elif timeRangeIndex == 2:
-        pass
+	kw, ht, img, lk = getInfoForDateRange('2014-02-19 00:00:00', '2014-02-25 23:59:59')
+    elif timeRangeIndex == 2: 
+	kw, ht, img, lk = getInfoForDateRange('2014-02-26 00:00:00', '2014-03-04 23:59:59')
     elif timeRangeIndex == 3:
-        pass
+	kw, ht, img, lk = getInfoForDateRange('2014-03-05 00:00:00', '2014-03-11 23:59:59')
     elif timeRangeIndex == 4:
-        pass
+	kw, ht, img, lk = getInfoForDateRange('2014-03-12 00:00:00', '2014-03-18 23:59:59')
     elif timeRangeIndex == 5:
-        pass
+	kw, ht, img, lk = getInfoForDateRange('2014-03-19 00:00:00', '2014-03-25 23:59:59')
     elif timeRangeIndex == 6:
-        pass
+	kw, ht, img, lk = getInfoForDateRange('2014-03-26 00:00:00', '2014-04-01 23:59:59')
     elif timeRangeIndex == 7:
-        pass
+	kw, ht, img, lk = getInfoForDateRange('2014-04-02 00:00:00', '2014-04-09 23:59:59')
     else:
-        # Return some kind of error message
-        pass
-    return render_template('timeRange.html', instance=instanceAddress, rangeIndex=timeRangeIndex)
-	
+    	kw = ['Error: No data in that time range']
+    	lk = kw
+	img = kw
+	ht = kw
+    return render_template('timeRange.html', instance=instanceAddress, rangeIndex=timeRangeIndex, topKeywords=kw, topHashtags=ht, topImages=img, topLinks=lk)
+
+@app.route("/tweetLocations")
+def getTweetLocations():
+	return json.dumps(tweetLocations)	
 
 if __name__ == '__main__':
     # Set to false before deployment
